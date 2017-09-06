fa8c4454a45a61012d4ad2204582549ca7a125d1
Arden Rasmussen <ardenisthebest@gmail.com>
2017-09-06 10:55:40 -0700

    Added arguments

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,39 @@
+#include "any_type.hpp"
 #include "parser.hpp"
 
-#include <iostream>
-
 int main(int argc, char const* argv[]) {
-  // std::cout << Red("Hello World\n");
-  /* code */
+  cli::Parser parser;
+  parser.SetVersion("Temporis-Cli v0.0");
+  parser.AddArgument(std::vector<std::string>{"-h", "--help"}, "help",
+                     "Displays this help mesage");
+  parser.AddArgument(std::vector<std::string>{"--version"}, "version",
+                     "Displays the version");
+  parser.ArgumentGroup("Timer Arguments");
+  parser.AddArgument("start", "start_bool", "set_true",
+                     "Prompts user for timer and starts it");
+  parser.AddArgument(std::vector<std::string>{"-s", "--start"}, "start",
+                     "Starts timmer for specified task");
+  parser.AddArgument("end", "end_bool", "set_true",
+                     "Prompts user for timer and ends it");
+  parser.AddArgument(std::vector<std::string>{"-e", "--end"}, "end",
+                     "Ends timer for specified task");
+  parser.AddArgument("end-all", "end_all", "set_true",
+                     "Ends all currently active timers");
+  parser.AddArgument("status", "status", "set_true",
+                     "Retuns list of running timmers");
+  parser.AddArgument("create", "create_bool", "set_true",
+                     "Prompts user for timer and creates new timmer");
+  parser.AddArgument(std::vector<std::string>{"-c", "--create"}, "create",
+                     "Creates a new timmer");
+  parser.AddArgument("delete", "delete_bool", "set_true",
+                     "Prompts user for timer and deletes timmer");
+  parser.AddArgument(std::vector<std::string>{"-d", "--delete"}, "delete",
+                     "Deletes a timmer");
+  parser.ArgumentGroup("Report Arguments");
+  parser.AddArgument(std::vector<std::string>{"-r", "--report"}, "report",
+                     "Returns report of given type [day, week, month, year]");
+  parser.AddArgument(std::vector<std::string>{"-p", "--project"}, "project",
+                     "Reports only time on designated project");
+  std::map<std::string, cli::Any> args = parser.ParseArgs(argc, argv);
   return 0;
 }
